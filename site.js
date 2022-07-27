9a74f169d9d3efbde96c44ab694bba1ec34d5044
Tej <tjoungieliejunior@gmail.com>
2022-07-27 17:48:52 +0100

    menu responsive

--- site.js
+++ site.js
@@ -1,3 +1,20 @@
+let body = document.querySelector("body")
+let toggle = document.querySelector(".toggle")
+let croix = true
+toggle.addEventListener("click", function hambuger() {
+    switch (croix) {
+        case true:
+            body.classList.add("open")
+            croix = false
+            break;
+    
+        case (false):
+            body.classList.remove("open")
+            croix = true
+            break;
+    }
+})
+
 let lien = document.querySelector("link")
 let imgageTheme = document.querySelector(".imgTheme")
 let theme = "light"
